84eb4f7da723288bab5fbea9985aea5eba7b75c6
Pavit Pimchanagul <pavitpim@gmail.com>
2023-04-06 12:00:14 +0700

    update

--- JS09-Array/06-modify-method/_demo/main.js
+++ JS09-Array/06-modify-method/_demo/main.js
@@ -4,29 +4,3 @@
 
 // console.log('Caller', arr);
 // console.log('Return', a);
-
-// LAB-1
-// const sports = ['football', 'volleyball', 'tennis', 'basketball', 'judo'];
-
-// const a = sports.slice(1, 4);
-// console.log('PARTIAL CLONE', a);
-// console.log('ORIGINAL ARR', sports);
-
-// const b = sports.splice(1, 3);
-// console.log('REMOVED ARR', b);
-// console.log('ORIGINAL ARR', sports);
-
-// LAB-2
-// let fish = ['angel', 'clown', 'mandarin', 'sturgeon'];
-
-// const a = fish.splice(0, fish.length);
-
-// console.log(fish);
-// console.log(a);
-
-// LAB-3
-const person = ['John', 'Jay', 'Jim', 'Jame'];
-
-person.splice(1, 1, 'Jack', 'Joe');
-
-console.log(person);
---
+++ JS09-Array/07-searching-method/_demo/main.js
@@ -0,0 +1,6 @@
+const arr = ['a', 'f', 'm', '0', 'j', 0, 'z', 'm'];
+
+let r = arr.includes(0, -2); // 8+(-3) == 5
+
+console.log(arr.length);
+console.log(r);
